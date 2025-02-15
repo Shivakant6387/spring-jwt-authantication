c94a2dcfbeac07efd79ed9211ee90aecf849da4c
shivakant singh <shivakant5875@gmail.com>
2025-02-15 11:21:41 +0530

    nothing to commit

---
+++ src/main/java/com/example/spring_boot_role_base_authentication/SpringBootRoleBaseAuthenticationApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring_boot_role_base_authentication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRoleBaseAuthenticationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootRoleBaseAuthenticationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/spring_boot_role_base_authentication/SpringBootRoleBaseAuthenticationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring_boot_role_base_authentication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootRoleBaseAuthenticationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
