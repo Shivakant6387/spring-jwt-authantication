9b9c135f7b099ae20cf7c34319e39840935443a4
shivakant singh <shivakant5875@gmail.com>
2025-05-06 14:16:59 +0530

    bind https and http

---
+++ src/main/java/com/example/spring_boot_role_base_authentication/config/WebServerConfig.java
@@ -0,0 +1,27 @@
+package com.example.spring_boot_role_base_authentication.config;
+
+import org.apache.catalina.connector.Connector;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class WebServerConfig {
+
+    @Bean
+    public ServletWebServerFactory servletContainer() {
+        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory();
+        tomcat.addAdditionalTomcatConnectors(createHttpConnector());
+        return tomcat;
+    }
+
+    private Connector createHttpConnector() {
+        Connector connector = new Connector(TomcatServletWebServerFactory.DEFAULT_PROTOCOL);
+        connector.setScheme("http");
+        connector.setPort(8443); // HTTP port
+        connector.setSecure(false);
+        connector.setRedirectPort(8445); // Redirect to HTTPS
+        return connector;
+    }
+}
